fe81c2ec81686ace509377b96341255ff72cca4a
joeand <joel.andersson@postkodlotteriet.se>
2017-01-11 13:49:31 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+var createRemotehostPreprocessor = function (args, config, logger, helper) {
+  config = config || {};
+
+  var log = logger.create('preprocessor.remotehost');
+  var defaultOptions = {
+    bare: true,
+    sourceMap: false
+  };
+  var options = helper.merge(defaultOptions, args.options || {}, config.options || {});
+  return function (content, file, done) {
+      function repl(str, p1, p2) {
+	  console.log("replacing str " + str + " matched groups: " + p1 + ", " + p2);
+	  return p1 + (config.hostname ? config.hostname : 'localhost') + '";';
+      }
+      var content_ = content.replace(/(const\shostname\s*=\s*\")(\w+\";)/, repl);
+      console.log("spec file content: " + content_);
+      try {
+	  done(content_);
+      } catch (e) {
+	  log.error('%s\n  at %s', e.message, file.originalPath);
+      }
+  }
+};
+
+createRemotehostPreprocessor.$inject = ['args', 'config', 'logger', 'helper'];
+
+// PUBLISH DI MODULE
+module.exports = {
+  'preprocessor:remotehost': ['factory', createRemotehostPreprocessor]
+};
